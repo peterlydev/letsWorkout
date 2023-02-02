b1bdd20ba52228528a2040085394cf7882ec28b1
Peter Ly <peter.ly1@hotmail.com>
2023-02-02 23:12:43 +0000

    created routers

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+});
 
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the app!'})
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port', process.env.PORT);
 });
+
+process.env
