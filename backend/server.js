31931458fd32a063964a751a4a08e51295d5a9cd
Peter Ly <peter.ly1@hotmail.com>
2023-02-04 21:07:32 +0000

    Testing Routes in insomnia

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const workouts = require('./routes/workouts');
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
