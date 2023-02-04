06a3d4cfbc066cfde3db0d2c9b7c5957d282b853
Peter Ly <peter.ly1@hotmail.com>
2023-02-04 20:36:32 +0000

    import routers to server file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const workouts = require('./routes/workouts');
 
 const app = express();
 
@@ -8,9 +9,7 @@ app.use((req, res, next) => {
     next()
 }); 
 
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app!'})
-})
+app.get(workouts);
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on port', process.env.PORT);
