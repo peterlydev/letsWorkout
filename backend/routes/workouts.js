d2e1923c74d7b33770b2e965c5de311e219905d2
Peter Ly <peter.ly1@hotmail.com>
2023-02-04 20:43:50 +0000

    adding route handlers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 const router = express.Router()
 
+//GET
 router.get('/', (req, res) => {
     res.json({mssg: 'GET ALL WORKOUTS!!!'})
 });
 
+
+//GET SINGLE WORKOUT
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET A SINGLE WORKOUT'})
+});
+
+//POST A NEW WORKOUT
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST A NEW WORKOUT'})
+});
+
+//DELETE A NEW WORKOUT
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE A WORKOUT'})
+});
+
+//UPDATE A NEW WORKOUT
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE A WORKOUT'})
+});
 module.exports = router;
