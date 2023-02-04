0762ce9ba1f6457b79370c90f48f749639cb4837
Peter Ly <peter.ly1@hotmail.com>
2023-02-04 21:31:43 +0000

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
-require('dotenv').config();
-const express = require('express');
-const workouts = require('./routes/workouts');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const workouts = require("./routes/workouts");
 
 const app = express();
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-}); 
-
-app.use('/api/workouts', workouts);
-
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT);
+  console.log(req.path, req.method);
+  next();
 });
 
-process.env
+app.use("/api/workouts", workouts);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB & Listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
