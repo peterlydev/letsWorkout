9e921de5e070070a430d6e0862c35b03a915b0be
Peter Ly <peter.ly1@hotmail.com>
2023-02-06 17:35:41 +0000

    Created router for login and signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,5 +2,14 @@ const express = require('express');
 
 const router = express.Router()
 
+//login
+router.post('/login', () => {
+
+})
+
+//signup
+router.post('/signup', () => {
+
+})
 
 module.exports = router
