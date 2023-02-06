2ec39c13065433370e6477108ee20395f8ef77f8
Peter Ly <peter.ly1@hotmail.com>
2023-02-06 20:51:02 +0000

    Added validation to email and password

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,6 +18,19 @@ const userSchema = new Schema({
 
 //static signup method
 userSchema.statics.signup = async function (email, password) {
+
+    //validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error('Email not valid')
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
     
     const exists = await this.findOne({ email })
 
