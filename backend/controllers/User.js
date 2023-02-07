ef27922ada49cbb70bfe5cabef7d88bd17e23641
Peter Ly <peter.ly1@hotmail.com>
2023-02-07 17:30:30 +0000

    create a jwt token

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,4 +1,9 @@
 const User = require("../models/User");
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 //login user
 const loginUser = async (req, res) => {
@@ -12,6 +17,8 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
+    //generate token
+    
     res.status(200).json({ email, user });
   } catch (error) {
     res.status(400).json({ error: error.message });
