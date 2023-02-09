41e09e2e98ced9d26837e0d5b2d2f36c703d607e
Peter Ly <peter.ly1@hotmail.com>
2023-02-09 14:02:11 +0000

    importing useSignup to the page

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -4,11 +4,12 @@ import { useSignup } from "../hooks/useSignup"
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { signup, error, isLoading } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    
   };
 
   return (
