1addcdb572bbb1e06c551903cb5497421a356e3a
Peter Ly <peter.ly1@hotmail.com>
2023-02-13 14:22:14 +0000

    created requireAuth file

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,3 +1,20 @@
+const jwt = require("jsonwebtoken");
 const requireAuth = (req, res, next) => {
+  //verify authentication
+  const { authorization } = req.headers;
 
-}
+  if (!authorization) {
+    return res.status(401).json({ error: "Authorisation token required" });
+  }
+
+  //we need the second item of the token
+  const token = authorization.split(" ")[1];
+
+  //verify token
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: "Request is not authorised" });
+  }
+};
