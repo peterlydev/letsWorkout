8ed014295a53b1946513fea5bfa110c608e0481b
Peter Ly <peter.ly1@hotmail.com>
2023-02-13 13:40:25 +0000

    Updating navbar by showing output

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,29 +4,33 @@ import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
-  const { logout } = useLogout()
-  const { user } = useAuthContext()
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
-    logout()
-  }
-  
+    logout();
+  };
+
   return (
     <header>
       <div className="container">
-        <Link to='/'>
-            <h1>Let's Workout</h1>
+        <Link to="/">
+          <h1>Let's Workout</h1>
         </Link>
         <nav>
-        <div>
-          <button onClick={handleClick}>Log out</button>
-        </div>
+        {user && (
           <div>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Signup</Link>
+            <span>{user.email}</span>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+        )}
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
           </div>
         </nav>
-      </div>;
+      </div>
+      ;
     </header>
   );
 };
